8437ccf9decf16028856089ac558018023a62e8c
ryan <ryan@ns3269875.ip-37-59-56.eu>
2014-04-27 12:04:25 +0100

    Fixed case sensitivity.

--- src/me/RyanWild/CJFreedomMod/CJFM_Util.java
+++ src/me/RyanWild/CJFreedomMod/CJFM_Util.java
@@ -1,27 +1,12 @@
 package me.RyanWild.CJFreedomMod;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
-import me.StevenLawson.TotalFreedomMod.TFM_Log;
-import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
-//import static me.StevenLawson.TotalFreedomMod.TotalFreedomMod.mySQL;
-//import net.coreprotect.CoreProtect;
-//import net.coreprotect.CoreProtectAPI;
-import org.bukkit.Bukkit;
-import static org.bukkit.Bukkit.getServer;
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 public class CJFM_Util
 {
 
-    public static final List<String> EXECUTIVES = Arrays.asList("camzie99", "phoenix411", "kyled1986", "andoodle");
+    public static final List<String> EXECUTIVES = Arrays.asList("Camzie99", "phoenix411", "Kyled1986", "andoodle");
     public static final List<String> SYSADMINS = Arrays.asList("wild1145", "varuct", "thecjgcjg", "darthsalamon");
     public static final List<String> DEVELOPERS = Arrays.asList("Madgeek1450", "DarthSalamon", "wild1145", "Paldiu", "MrPorkSausage", "Camzie99");
 
