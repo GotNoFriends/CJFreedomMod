e8b67645ccfcf3d50f12f400711addca40742bcb
hawkeyeshi <hawkeyeshi6103@gmail.com>
2014-05-19 22:37:33 +0000

    Fixed Command_d

--- src/me/RyanWild/CJFreedomMod/Commands/Command_d.java
+++ src/me/RyanWild/CJFreedomMod/Commands/Command_d.java
@@ -1,10 +1,11 @@
-
 package me.RyanWild.CJFreedomMod.Commands;
 
 import me.RyanWild.CJFreedomMod.CJFM_Util;
 import me.RyanWild.CJFreedomMod.Player.CJFM_DonatorList;
 import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -26,17 +27,24 @@ public class Command_d extends CJFM_Command
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
-        if(args.length > 1)
-            return false;
         
-        if(!CJFM_DonatorList.isSuperDonor(sender) || !TFM_AdminList.isSeniorAdmin(sender))
-            return false; 
-        
-        if(args.length == 1)
+        if(args.length == 0)
         {
-            CJFM_Util.donatorChatMessage(sender, StringUtils.join(args, " "), senderIsConsole);
+            return false;
         }
-                
+        else
+        {
+            if(TFM_AdminList.isSeniorAdmin(sender) || CJFM_DonatorList.isSuperDonor(sender))
+            {
+                CJFM_Util.donatorChatMessage(sender, StringUtils.join(args, " "), senderIsConsole);
+            }
+            else
+            {
+                TFM_Util.playerMsg(sender, "You do not have permission to access Donator Chat!", ChatColor.RED);
+                return true;
+            }
+        }
         return false;
+
     }
 }
