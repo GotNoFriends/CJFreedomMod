f04b15fd2bd7677e791b7e15f8f0021a2fdcb13d
Ryan <Admin@superior-networks.com>
2014-05-19 13:39:46 +0100

    Added the last part of BarAPI. Resolves #23

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_denick.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_denick.java
@@ -2,6 +2,8 @@ package me.StevenLawson.TotalFreedomMod.Commands;
 
 import me.StevenLawson.TotalFreedomMod.Bridge.TFM_EssentialsBridge;
 import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import me.confuser.barapi.BarAPI;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -14,6 +16,7 @@ public class Command_denick extends TFM_Command
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
     {
         TFM_Util.adminAction(sender.getName(), "Removing all nicknames", false);
+        BarAPI.setMessage(ChatColor.RED + "" + ChatColor.BOLD + sender.getName() + " Has removed all nicknames from the server. ", 60);
 
         for (Player player : server.getOnlinePlayers())
         {
